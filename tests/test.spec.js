0ef24ac0b34e7d667e2734407c8629a5e7ee2a12
lastrites2018 <lastrites@nate.com>
2023-05-31 19:23:45 +0900

    package.json 설정 및 yaml 업데이트

---
+++ tests/test.spec.js
@@ -0,0 +1,21 @@
+import { test } from "@playwright/test";
+
+let page;
+test.describe.configure({ mode: "serial" });
+
+test.beforeAll(async ({ browser, baseURL }) => {
+  page = await browser.newPage();
+  if (!baseURL) {
+    test.fail(true, "playwright.config.ts 에서 baseURL 설정이 필요합니다.");
+    return;
+  }
+  await page.goto(baseURL);
+});
+
+test.describe("테스트 1", () => {
+  test("테스트 코드", async () => {
+    const headerExist = page.locator("App-header").isVisible();
+
+    await test.fail(!headerExist, "헤더가 없습니다.");
+  });
+});
