e81109faf97a8ee7ec7529cad24e008ba731a400
lastrites2018 <lastrites@nate.com>
2023-05-31 19:47:43 +0900

    스펙 변경

--- tests/test.spec.js
+++ tests/test.spec.js
@@ -5,11 +5,11 @@ test.describe.configure({ mode: "serial" });
 
 test.beforeAll(async ({ browser, baseURL }) => {
   page = await browser.newPage();
-  if (!baseURL) {
-    test.fail(true, "playwright.config.ts 에서 baseURL 설정이 필요합니다.");
-    return;
-  }
-  await page.goto(baseURL);
+  // if (!baseURL) {
+  //   // test.fail(true, "playwright.config.ts 에서 baseURL 설정이 필요합니다.");
+  //   return;
+  // }
+  // await page.goto(baseURL);
 });
 
 test.describe("테스트 1", () => {
