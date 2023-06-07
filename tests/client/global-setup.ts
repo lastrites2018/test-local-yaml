ef69444b7d3d3d9701b1c9ecf56b3fb44d22cf13
lastrites2018 <lastrites@nate.com>
2023-06-07 19:21:43 +0900

    시간 변경 및 global setup 변경

--- tests/client/global-setup.ts
+++ tests/client/global-setup.ts
@@ -5,6 +5,7 @@ const pwd = process.env.REACT_APP_CLIENT_PWD || "";
 const waitingApiResponseUrl = "**/auth/login";
 
 const loginURL = process.env.loginURL;
+console.log("loginURL: ", loginURL);
 
 async function globalSetup(config: FullConfig) {
   const { baseURL, storageState } = config.projects[0].use;
@@ -22,12 +23,15 @@ async function globalSetup(config: FullConfig) {
       .filter({ has: page.locator("span") })
       .click(),
   ]);
-  await page.waitForTimeout(2000);
+  //   await page.waitForTimeout(2000);
+  await page.waitForTimeout(6000);
+  console.log("로그인 됐을까?");
   await page
     .getByRole("button", { name: "감정평가 의뢰", exact: true })
     .filter({ has: page.locator("span") })
     .click();
-  await page.waitForTimeout(2000);
+  //   await page.waitForTimeout(2000);
+  await page.waitForTimeout(6000);
   await page.context().storageState({ path: storageState as string });
   await browser.close();
 }
